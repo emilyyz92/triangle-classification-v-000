b0a1c99698bbfb6f78deec024f86e9c9b8bed917
Emily Zhao <tong.zhao.92@gmail.com>
2017-12-06 01:27:37 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,11 +1,15 @@
 class Triangle
   attr_accessor :kind
   def initialize(a,b,c)
-    if a==b && b==c
+    if a>0 && b>0 && c>0
+      a==b && b==c
       self.kind = :equilateral
     elsif a!=b && b!=c
       self.kind = :scalene
     elsif a==c && b!=c
       self.kind = :isosceles
+
+  class TriangleError < StandardError
+  end
 
 end
