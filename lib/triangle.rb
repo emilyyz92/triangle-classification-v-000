99f17370245b652fecb3533a1b7651f6645bb7c4
Emily Zhao <tong.zhao.92@gmail.com>
2017-12-06 02:01:39 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,7 +3,7 @@ class Triangle
 
   def initialize(a,b,c)
     if a>0 && b>0 && c>0
-      if a+b>c || b+c>a || a+c>b
+      if a+b>c && b+c>a && a+c>b
         if a==b && b==c
           self.kind = :equilateral
         elsif a!=b && b!=c && a!=c
